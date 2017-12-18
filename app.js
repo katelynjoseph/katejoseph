5b620963421d065d3774d5fe3cacb7b4b87b7dda
Katelyn Joseph <katelynmcconnell6@gmail.com>
2017-12-17 19:07:59 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var apos = require('apostrophe')({
+  shortName: 'brokenmirror',
+  title: 'brokenmirror',
+
+  // See lib/modules for basic project-level configuration of our modules
+  // responsible for serving static assets, managing page templates and
+  // configuring user acounts.
+
+  modules: {
+    // Add custom apostrophe-modules and their respective configuration here!
+	'link-widgets': {}
+  }
+});
+
+
